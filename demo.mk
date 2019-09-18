cacfa1abc7556d447e63ac5ec1c325aae6cc2c9f
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2019-09-18 13:47:40 +0200

    Complete demo.mk

--- demo.mk
+++ demo.mk
@@ -1,4 +1,12 @@
 DOCKER_PORT=8487
 DOCKER_TAG=demo
+PROXY_PREFIX=/getitfixed
 
 include Makefile
+
+docker-compose-env: ## Build docker-compose environment file
+	rm -f .env
+	touch .env
+	sleep 0.1
+	touch .env.mako
+	$(DOCKER_MAKE_CMD) .env
