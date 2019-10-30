a67f7ac132735546f22c5734aca75e2ba2e3d332
Antoine Abt <antoine@abt.im>
2019-10-30 09:39:08 +0100

    refactor: simplify code

--- getitfixed/static/scripts/legend.js
+++ getitfixed/static/scripts/legend.js
@@ -1,26 +1,8 @@
-const generateLegend = function(features) {
-  const results = []
-  const map = new Map()
-  for (const feature of features) {
-    let category = feature.get("category")
-    if (!map.has(category)) {
-      map.set(category, true)
-      results.push({
-        cat: category,
-        icon: feature.get("icon")
-      })
-    }
-  }
-  results.sort((a, b) => (a.cat > b.cat ? 1 : b.cat > a.cat ? -1 : 0))
-  let tbody = $("#feature-legend .list-group")
-  $.each(results, function(i, result) {
-    let li = $("<li>")
-    $("<li>", { class: "list-group-item" })
-      .html(result["cat"])
-      .append($("<img>").attr("src", result["icon"]))
-      .appendTo(li)
-      .appendTo(li)
-
-    tbody.append(li)
-  })
+const generateLegend = features => {
+  const cats = new Map()
+  features.forEach(f => cats.set(f.get("category"), f.get("icon")))
+  let tbody = $(".feature-legend .list-group")
+  new Map([...cats.entries()].sort()).forEach(
+    (icon, cat) => tbody.append(`<li class="list-group-item"><img src="${icon}"/>${cat}</li>`)
+  )
 }
