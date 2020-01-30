3418e09d271571a22f58acdad27eb19647d72553
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-01-30 16:51:18 +0100

    Add root includeme

--- getitfixed/__init__.py
+++ getitfixed/__init__.py
@@ -1,5 +1,5 @@
-from pyramid.config import Configurator
 from pkg_resources import resource_filename
+from pyramid.config import Configurator
 
 from c2c.template.config import config as configuration
 import c2cgeoform
@@ -13,26 +13,24 @@ c2cgeoform.default_search_paths = search_paths
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    # Update the settings object from the YAML application config file
+    configuration.init(settings.get("app.cfg"))
+    settings.update(configuration.get_config())
+
     config = Configurator(
         settings=settings, locale_negotiator="getitfixed.i18n.locale_negotiator"
     )
 
-    # Update the settings object from the YAML application config file
-    configuration.init(settings.get("app.cfg"))
-    config.get_settings().update(configuration.get_config())
-
-    # use pyramid_tm to hook the transaction lifecycle to the request
-    config.include("pyramid_tm")
-
-    # config.include('c2cwsgiutils.pyramid.includeme')
-    config.include("pyramid_jinja2")
-    config.include("c2cgeoform")
-    config.include(".models")
-    config.include(".routes")
+    config.include("getitfixed")
+    config.include("getitfixed.models")
     c2cgeoform.routes.register_routes(config)
 
-    config.add_translation_dirs("locale")
-
-    config.scan()
-
     return config.make_wsgi_app()
+
+
+def includeme(config: Configurator):
+    config.include("pyramid_jinja2")
+    config.include("c2cgeoform")
+    config.include(".routes")
+    config.add_translation_dirs("getitfixed:locale")
+    config.scan()
