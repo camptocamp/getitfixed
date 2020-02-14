5e21d634c6eb0b2f58227e3b6d33fceca9eb07c7
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-02-14 11:09:20 +0100

    Do not register not found view when used as a plugin

--- getitfixed/__init__.py
+++ getitfixed/__init__.py
@@ -25,6 +25,11 @@ def main(global_config, **settings):
     config.include("getitfixed.models")
     c2cgeoform.routes.register_routes(config)
 
+    def notfound_view(request):
+        request.response.status = 404
+        return {}
+    config.add_notfound_view(notfound_view, renderer="getitfixed:templates/404.jinja2")
+
     return config.make_wsgi_app()
 
 
