93c00f3af08a1c5da504038ed500640c0efc043f
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2021-02-08 11:08:11 +0100

    Try customizable routes

--- getitfixed/routes.py
+++ getitfixed/routes.py
@@ -3,12 +3,31 @@ from getitfixed.models.getitfixed import Event, Issue
 
 
 def includeme(config):
+    getitfixed_settings = config.get_settings()["getitfixed"]
+
+    routes = {
+        **{
+            "getitfixed": "getitfixed",
+            "getitfixed_private": "getitfixed_private",
+            "getitfixed_admin": "getitfixed_admin",
+        }
+        **getitfixed_settings.get("routes", {}),
+    }
+
     config.add_static_view("getitfixed_static", "getitfixed:static", cache_max_age=3600)
 
     config.add_route("getitfixed", "/getitfixed/")
-    config.add_c2cgeoform_application("getitfixed", [("issues", Issue)])
+    config.add_c2cgeoform_application(
+        "getitfixed",
+        [("issues", Issue)],
+        segment_url="signalez-nous",
+    )
 
-    config.add_c2cgeoform_application("getitfixed_private", [])
+    config.add_c2cgeoform_application(
+        "getitfixed_private",
+        [],
+        segment_url=routes["getitfixed_private"],
+    )
 
     # Temporary redirect URLs sent by email to new URL
     config.add_route(
@@ -17,9 +36,11 @@ def includeme(config):
         pregenerator=pregenerator,
     )
 
-    config.add_route("getitfixed_admin", "/getitfixed_admin/")
+    config.add_route("getitfixed_admin", "/{}_admin/".format())
     config.add_c2cgeoform_application(
-        "getitfixed_admin", [("issues", Issue), ("event", Event)]
+        "getitfixed_admin",
+        [("issues", Issue), ("event", Event)],
+        segment_url=routes["getitfixed_admin"],
     )
     config.add_route(
         "getitfixed_set_private",
