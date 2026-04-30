a4e6313ac4ff14591d1195b8b36454cc175e5565
Alexian Masson <12852383+AlexianMasson@users.noreply.github.com>
2026-04-30 14:20:33 +0200

    Fix pytest 9 deprecation: remove usefixtures marks on fixtures

--- acceptance_tests/private/issue_test.py
+++ acceptance_tests/private/issue_test.py
@@ -9,7 +9,6 @@ from unittest.mock import patch, call, ANY
 
 
 @pytest.fixture(scope="function")
-@pytest.mark.usefixtures("dbsession", "transact")
 def issue_test_data(dbsession, transact):
     del transact
 
